d715d29c01fa8ef700955835d1ef621d882d53ee
Dimas-Adyani <18106050036@student.uin-suka.ac.id>
2021-04-28 20:29:12 +0700

    menghapus scaffold style dan menghapus field id pada form product di view/product

--- db/migrate/20210428130927_create_products.rb
+++ db/migrate/20210428130927_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-      t.primary_key :id
       t.string :title
       t.string :price
       t.text :description
